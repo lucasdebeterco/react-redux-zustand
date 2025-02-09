59242175b67735a3d1662abc846a82ee286eb715
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2025-02-09 10:59:55 -0300

    listing todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Provider as ReduxProvider } from "react-redux"
+import { TodoList } from "./components/TodoList.tsx"
+import { AddTodo } from "./components/AddTodo.tsx"
+import { store } from "./store"
+
 export function App() {
   return (
-    <>
-      aaaa
-    </>
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
   )
 }
-
-
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: 'Todo',
+  initialState: ['Gym', 'Study Redux'],
+  reducers: {}
+})
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    todo: todoSlice.reducer
+  }
 })
