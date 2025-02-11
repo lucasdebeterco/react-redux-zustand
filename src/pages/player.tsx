7351edfabe8173a51ee44e93a2244bf9533799b2
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2025-02-10 23:01:18 -0300

    redux player project config

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -1,25 +1,43 @@
-import { MessageCircle } from 'lucide-react'
+import { MessageCircle } from "lucide-react"
+
+import { Header } from "../components/header"
+import { Video } from "../components/video"
+import { Module } from "../components/module"
+import { useAppSelector } from "../store"
 
 export function Player() {
+
+  const modules = useAppSelector(state => {
+    return state.player.course.modules
+  })
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
         <div className="flex items-center justify-between">
-          {/* Header */}
-          <div className="flex flex-col gap-1">
-            <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-            <span className="text-sm text-zinc-400">Módulo "Desvendando o Redux"</span>
-          </div>
+          <Header />
+
           <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
             <MessageCircle className="w-4 h-4" />
             Deixar feedback
           </button>
         </div>
-        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
+
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
           <div className="flex-1">
-            video
+            <Video />
           </div>
-          <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]">
+          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll">
+            {modules.map((module, index) => {
+              return (
+                <Module
+                  key={module.id}
+                  moduleIndex={index}
+                  title={module.title}
+                  amountOfLessons={module.lessons.length}
+                />
+              )
+            })}
           </aside>
         </main>
       </div>
