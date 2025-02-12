b6c45d7b5378b85e8c85c66fbf3c1119c7de534c
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2025-02-11 23:04:01 -0300

    selecting lessons

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -1,8 +1,10 @@
-import * as Collapsible from '@radix-ui/react-collapsible'
+import * as Collapsible from '@radix-ui/react-collapsible';
 
-import { ChevronDown } from "lucide-react"
-import { Lesson } from "./lesson"
+import { ChevronDown } from "lucide-react";
+import { Lesson } from "./Lesson";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface ModuleProps {
   moduleIndex: number;
@@ -11,6 +13,13 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const dispatch = useDispatch()
+
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })
 
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
@@ -33,12 +42,17 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex &&
+              currentLessonIndex === lessonIndex
+
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                isCurrent={isCurrent}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,13 +1,22 @@
 import ReactPlayer from 'react-player'
+import {useAppSelector} from "../store";
 
 export function Video() {
+
+  const lesson = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+  })
+
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
